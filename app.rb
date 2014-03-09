8d0179e1550e81679edbeee17cd377b135120032
Kevin Hughes <kevin.hughes@shopify.com>
2014-03-09 19:51:43 -0400

    fulfillment service integrator initial commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require "uri"
+require 'net/http'
+
+get '/' do
+  "Shopify Fulfillment Integration"
+end
+
+def forward_request
+  request_bin = "/132ahma1"
+  uri = URI.parse("http://requestb.in" + request_bin)
+  uri.query = URI.encode_www_form(params)
+  response = Net::HTTP.get_response(uri)
+end
+
+get '/fetch_stock' do
+  forward_request
+end
+
+get '/fetch_tracking_numbers' do
+  forward_request
+end
