8e6174b67f410c10caf87a03a8a2f5dbd2a30d9c
Kevin Hughes <kevin.hughes@shopify.com>
2014-03-11 20:21:02 -0400

    remove forwarding to request bin and respond with a static but valid response to shopify

--- app.rb
+++ app.rb
@@ -9,21 +9,41 @@ get '/' do
   erb :index, :locals => {:log => log.reverse} 
 end
 
-# fetch_stock endpoint
-get '/fetch_stock.json' do
-  forward_request
+# /fetch_stock
+# Listen for a request from Shopify
+# When a request is recieved make a request to fulfillment service
+# Parse response from fulfillment service
+# Respond to Shopify
+#
+# Example of a Shopify request:
+# https://myapp.com/fetch_stock?sku=123&shop=testshop.myshopify.com
+#
+get '/fetch_stock' do
+  sku = params["sku"]
+  shop = params["shop"]
+
+  content_type :json
+  { sku => 11 }.to_json
 end
 
-# fetch_tracking_numbers endpoint
-get '/fetch_tracking_numbers.json' do
-  forward_request
-end
+# /fetch_tracking_numbers
+# Listen for a request from Shopify
+# When a request is recieved make a request to fulfillment service
+# Parse response from fulfillment service
+# Respond to Shopify
+#
+# Example of a Shopify request:
+# http://myapp.com/fetch_tracking_numbers?order_ids[]=1&order_ids[]=2&order_ids[]=3
+# 
+get '/fetch_tracking_numbers' do
+  order_ids = params["order_ids"]
+  tracking_numbers = Hash[order_ids.map {|x| [x, "12345"]}]
 
-def forward_request
-  request_bin = "/132ahma1"
-  uri = URI.parse("http://requestb.in" + request_bin)
-  uri.query = URI.encode_www_form(params)
-  response = Net::HTTP.get_response(uri)
+  content_type :json
+  { "tracking_numbers" => tracking_numbers,
+    "message" => "Successfully received the tracking numbers",
+    "success" => true
+  }.to_json
 end
 
 # Log the request
